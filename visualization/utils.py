Fixed a huge issue which caused a lot of problems with the learning. By adding normalization using StandardScaler from sklearn.preprocessing in the preprocessing step, I was able to get the centralized learning close to 100 % accuracy, the FL learning to 96 %~, and the FL-DP to 94 %~ accuracy. All of this was done with a way more simple model (two hidden dense layers with 90 units).
This matches the accuracy that the original paper got in their centralized learning, with this same model. So, the reason I did this is that BatchNormalization isn't consistent with DP training [https://arxiv.org/pdf/2006.10919].

Anyway, I also updated the visualization and everything else to fully support DP.

visualization/utils.py
@@ -1,14 +1,27 @@
 import numpy as np
-import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
-from apps.task import one_hot_encode
+from apps.task import one_hot_encode, load_model
 from data_loading.data_loader import load_data
+from pathlib import Path
+import json
 
 
-def load_predictions_and_classes(model_path, test_data_path):
+def load_run_config(run_dir: Path):
+    return json.load((run_dir / "run_config.json").open())
+
+
+def load_predictions_and_classes(model_path, test_data_path, run_cfg):
+    # Load data
     df_test, x_test, y_test = load_data(test_data_path)
 
-    model = tf.keras.models.load_model(model_path)
+    # Variables
+    use_dp = run_cfg["use-dp"]
+    l2_norm_clip = run_cfg["l2-norm-clip"]
+    noise_multiplier = run_cfg["noise-multiplier"]
+    dp_args = dict(use_dp=use_dp, l2_norm_clip=l2_norm_clip, noise_multiplier=noise_multiplier)
+    model = load_model(**dp_args)
+
+    model.load_weights(model_path)
     predictions = model.predict(x_test)
 
     predicted_classes, true_classes = one_hot_encode(y_test, predictions)